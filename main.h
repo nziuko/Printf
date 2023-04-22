f3ad1e0f84b816a552cbb05554cd4eef466df544
nziuko <catherine.nziuko@gmail.com>
2023-04-22 02:52:37 -0700

    I'm not going anywhere

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef _FILE_NAME_H_
 #define _FILE_NAME_H_
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
-#endif
+va_list list, char buffer[], int flags, int width, int precision, int size
+#endif /* MAIN_H */
