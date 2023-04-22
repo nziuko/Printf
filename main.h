4cc06a1767e841d24e680e82a93f9b2a9d85bcbb
nziuko <catherine.nziuko@gmail.com>
2023-04-22 01:30:23 -0700

    I'm not going anywhere

--- main.h
+++ main.h
@@ -1,4 +1,6 @@
 #ifndef _FILE_NAME_H_
 #define _FILE_NAME_H_
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 #endif
