990aba008d97571015d282580b30dccee9f1f93e
nziuko <catherine.nziuko@gmail.com>
2023-04-22 10:20:01 -0700

    Update

--- main.h
+++ main.h
@@ -1,12 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
+/* Macro definitions */
+#define BUFF_SIZE 1024
 
+/* Function prototypes */
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/* Helper function prototypes */
+int handle_print(const char *format, int *i, va_list list, char buffer[],
+int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+void print_buffer(char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
+
