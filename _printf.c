ffd6d3163f4401025a4a4a1dbbacc49d96b9f754
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-24 05:04:33 -0700

    fixed return value of %

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL || (*format == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 	k = printthis(format, args);
 	va_end(args);
