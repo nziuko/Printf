d5733316e6348e6dbee6162d2bb33409cfc97c1a
nziuko <catherine.nziuko@gmail.com>
2023-04-24 08:46:28 -0700

    tests

--- tests/main.c
+++ tests/main.c
@@ -7,18 +7,17 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 int i, j, k, l, m, n, o, p;
-i = _printf("hello world%s\n", NULL);
-j = printf("hello world%s\n", NULL);
-k = printf("bye%c\n", NULL);
-l = _printf("bye%c\n", NULL);
-m = printf("we%\ncool\n");
-n = _printf("we%\ncool\n");
-o = printf("ok%%%%");
-p = _printf("ok%%%%");
+i = _printf("\%");
+j = _printf("yousef%s\n", "sherif");
+k = _printf("%%s", NULL);
+l = _printf("\nbye\n", NULL);
+m = _printf("%s goal\n", "scored");
+n = _printf("we\%\ncool\n");
+o = _printf("catherine%\n");
+p = _printf("%");
 printf("%d %d %d %d %d %d %d %d\n", i, j, k, l, m, n, o, p);
 return (0);
 }
