da757a3977bfcd7aabbc0cb785fd159f51679a4b
nziuko <catherine.nziuko@gmail.com>
2023-04-25 05:48:39 -0700

    stringhandler

---
+++ stringhandler.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * stringhandler - handle strings %s
+ * @args: arguments
+ * Return:  number of characters printed
+ */
+
+int stringhandler(va_list args);
+{
+int i = 0;
+char *s;
+s = va_arg(args, char*);
+if (s == NULL)
+s = "(null)";
+for (; *s != '\0'; s++)
+i += _putchar(*s);
+return (i);
+}
