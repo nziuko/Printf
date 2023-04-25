026b54d098a5c8d2e7512a4582967cac3308bd3e
nziuko <catherine.nziuko@gmail.com>
2023-04-25 05:23:42 -0700

    charhandler

---
+++ charhandler.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * charhandler - handle characters %c
+ * @args: arguments
+ * Return:  number of characters printed
+ */
+
+int charhandler(va_list args);
+{
+char ch;
+int i = 0;
+ch = va_arg(args, int);
+i += _putchar(ch);
+return (i);
+}
